d63b42f272692d3b946ab74959841b8a47c0802b
budosen <panjidia995@gmail.com>
2020-07-01 23:27:19 +0700

    test multi layer splited

--- layer/kavling.php
+++ layer/kavling.php
@@ -17,14 +17,14 @@ foreach ($res as $key => $sult) {
 		'<img src = \"https://scontent-sin6-2.xx.fbcdn.net/v/t1.0-9/91400513_1553639021452975_5524976870795247616_n.png?_nc_cat=110&_nc_sid=730e14&_nc_eui2=AeF3Z5XcreiARhaAIR3lFBk8i4S9cKEyQ4KLhL1woTJDgjW1y9_p-osrlqnLwrxBLxlX95MDOg9MvMSQ2wqcufoN&_nc_ohc=w6KoWgus7VAAX-dK1mL&_nc_ht=scontent-sin6-2.xx&oh=1b5cea5b065fe627c23d040f57ab790a&oe=5F166FF5\"/>' +
 		'</div>'+
 		'<div class=\"column\">".$sult['NamaProperti']."</div>'+
-	'</div>';";
+	'</div>';\n";
 
 	
 	echo "var customOptions_kav_".$count." =
         {
         'maxWidth': '500',
         'className' : 'custom_kav_".$count,"'
-        }";
+        }\n";
     echo "L.marker([".$sult['Latitude'].",".$sult['Longitude']."]).bindPopup(part_kav_".$count,", customOptions_kav_".$count.").addTo(kavling);";
 
     $count++;
