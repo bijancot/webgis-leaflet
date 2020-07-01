7213c35f4ae601d5a0371c801fd4069c1c4b2ec1
budosen <panjidia995@gmail.com>
2020-07-01 20:22:07 +0700

    test multi layer splited

---
+++ conn/koneksi.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "admin_gis";
+$password = "1sampaigis";
+$dbname = "admin_gis";
+
+// Create connection
+$mysqli = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($mysqli->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+?>
--- layer/kavling.php
+++ layer/kavling.php
@@ -1,4 +1,17 @@
 <?php
+
+include_once('../conn/koneksi.php');
+
+
+$db = $mysqli->prepare("SELECT * FROM properti a join jenisProperti b on a.JenisID=b.JenisID where a.JenisID = '?'");
+$db->bind_param("s",$jenis);
+
+$jenis = "P002";
+$db->execute();
+$db->fetchAll();
+
+var_dump($db);
+
 echo "L.marker([-7.900509, 112.6069513]).bindPopup(part, customOptions).addTo(kavling),
 L.marker([-7.905609, 112.6122395]).bindPopup(part, customOptions).addTo(kavling),
 L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.').addTo(kavling),
