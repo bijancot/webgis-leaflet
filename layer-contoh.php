ec0485fb8d45ea1003c36c842f67855be21165a0
budosen <panjidia995@gmail.com>
2020-07-01 20:46:09 +0700

    test multi layer splited

--- layer-contoh.php
+++ layer-contoh.php
@@ -75,7 +75,7 @@
 	L.marker([-7.900509, 112.6069513]).bindPopup(part, customOptions).addTo(kavling),
 	L.marker([-7.905609, 112.6122395]).bindPopup(part, customOptions).addTo(kavling),
 	L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.').addTo(kavling),
-	L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.').addTo(kavling);
+	L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.').addTo(kavling),
 
 	var apart= L.layerGroup();
 
