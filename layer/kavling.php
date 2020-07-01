5e0997803141e984454921d16b3b951bc7d69243
budosen <panjidia995@gmail.com>
2020-07-01 20:56:37 +0700

    test multi layer splited

--- layer/kavling.php
+++ layer/kavling.php
@@ -1,19 +1,16 @@
 <?php
-
-include_once('../conn/koneksi.php');
-
-
-$db = $mysqli->prepare("SELECT * FROM properti a join jenisProperti b on a.JenisID=b.JenisID where a.JenisID = '?'");
-$db->bind_param("s",$jenis);
+require('../conn/koneksi.php');
 
 $jenis = "P002";
+$db = $mysqli->prepare("SELECT * FROM properti a join jenisProperti b on a.JenisID=b.JenisID where a.JenisID = ?");
+$db->bind_param("s",$jenis);
 $db->execute();
-$db->fetchAll();
 
-var_dump($db);
+$cos = $db->get_result();
+$res = $cos->fetch_all(MYSQLI_ASSOC);
 
-echo "L.marker([-7.900509, 112.6069513]).bindPopup(part, customOptions).addTo(kavling),
-L.marker([-7.905609, 112.6122395]).bindPopup(part, customOptions).addTo(kavling),
-L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.').addTo(kavling),
-L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.').addTo(kavling);";
+sizeof($res);
+foreach ($res as $key => $sult) {
+    echo "L.marker([".$sult['Latitude'].",".$sult['Longitude']."]).bindPopup(part, customOptions).addTo(kavling);";
+}
 ?>
