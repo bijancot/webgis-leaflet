a6bd32bff8af7e3f07cbeef89dce482ef4aef5e2
budosen <panjidia995@gmail.com>
2020-07-01 23:25:50 +0700

    test multi layer splited

--- layer/kavling.php
+++ layer/kavling.php
@@ -9,8 +9,10 @@ $db->execute();
 
 $cos = $db->get_result();
 $res = $cos->fetch_all(MYSQLI_ASSOC);
+
+$count = 0;
 foreach ($res as $key => $sult) {
-    echo "var part_kav_ = '<h4>Judul </h4><div class=\"columns\">'+
+    echo "var part_kav_ = '<h4>Judul".$count."</h4><div class=\"columns\">'+
 		'<div class=\"column\">' +
 		'<img src = \"https://scontent-sin6-2.xx.fbcdn.net/v/t1.0-9/91400513_1553639021452975_5524976870795247616_n.png?_nc_cat=110&_nc_sid=730e14&_nc_eui2=AeF3Z5XcreiARhaAIR3lFBk8i4S9cKEyQ4KLhL1woTJDgjW1y9_p-osrlqnLwrxBLxlX95MDOg9MvMSQ2wqcufoN&_nc_ohc=w6KoWgus7VAAX-dK1mL&_nc_ht=scontent-sin6-2.xx&oh=1b5cea5b065fe627c23d040f57ab790a&oe=5F166FF5\"/>' +
 		'</div>'+
@@ -18,12 +20,13 @@ foreach ($res as $key => $sult) {
 	'</div>';";
 
 	
-	echo "var customOptions_kav =
+	echo "var customOptions_kav_".$count." =
         {
         'maxWidth': '500',
-        'className' : 'custom_kav'
+        'className' : 'custom_kav_".$count,"'
         }";
-    echo "L.marker([".$sult['Latitude'].",".$sult['Longitude']."]).bindPopup(part_kav_, customOptions_kav).addTo(kavling);";
+    echo "L.marker([".$sult['Latitude'].",".$sult['Longitude']."]).bindPopup(part_kav_".$count,", customOptions_kav_".$count.").addTo(kavling);";
 
+    $count++;
 }
 ?>
