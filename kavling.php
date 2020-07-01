35284a91803a5521cfa7648acb71d592181eef55
budosen <panjidia995@gmail.com>
2020-07-01 20:28:07 +0700

    test multi layer splited

--- kavling.php
+++ kavling.php
@@ -1,5 +1,9 @@
 <?php
 
+echo "L.marker([-7.900509, 112.6069513]).bindPopup(part, customOptions).addTo(kavling),
+L.marker([-7.905609, 112.6122395]).bindPopup(part, customOptions).addTo(kavling),
+L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.').addTo(kavling),
+L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.').addTo(kavling);";
 include_once('conn/koneksi.php');
 
 
