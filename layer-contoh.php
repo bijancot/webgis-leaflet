57277585e181346e1cf3b020982fa58b3526bbbf
budosen <panjidia995@gmail.com>
2020-07-01 16:43:50 +0700

    test multi layer splited

--- layer-contoh.php
+++ layer-contoh.php
@@ -75,6 +75,8 @@
 	// L.marker([-7.905609, 112.6122395]).bindPopup(part, customOptions).addTo(kavling),
 	// L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.').addTo(kavling),
 	// L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.').addTo(kavling);
+		// variabel layergroup
+	var kavling= L.layerGroup();
 	<?php include_once('layer/kavling.php')?>
 
 	var apart= L.layerGroup();
