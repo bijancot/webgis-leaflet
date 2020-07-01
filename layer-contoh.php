e84367e1df7405038be68b2d8a0670f4569d1ca8
budosen <panjidia995@gmail.com>
2020-07-01 21:24:21 +0700

    test multi layer splited

--- layer-contoh.php
+++ layer-contoh.php
@@ -79,7 +79,20 @@
 	
 	<?php 
 		echo "panji";
-		require './layer/kavling.php';
+		require '../conn/koneksi.php';
+
+		$jenis = "P002";
+		$db = $mysqli->prepare("SELECT * FROM properti a join jenisProperti b on a.JenisID=b.JenisID where a.JenisID = ?");
+		$db->bind_param("s",$jenis);
+		$db->execute();
+
+		$cos = $db->get_result();
+		$res = $cos->fetch_all(MYSQLI_ASSOC);
+
+		sizeof($res);
+		foreach ($res as $key => $sult) {
+			echo "L.marker([".$sult['Latitude'].",".$sult['Longitude']."]).bindPopup(part, customOptions).addTo(kavling);";
+		}
 	?>
 
 	var apart= L.layerGroup();
