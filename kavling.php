e3ceddda01e194651fb149e4e0d3c709960a60a3
budosen <panjidia995@gmail.com>
2020-07-01 20:55:12 +0700

    test multi layer splited

--- kavling.php
+++ kavling.php
@@ -1,9 +1,4 @@
 <?php
-
-echo "L.marker([-7.900509, 112.6069513]).bindPopup(part, customOptions).addTo(kavling),
-L.marker([-7.905609, 112.6122395]).bindPopup(part, customOptions).addTo(kavling),
-L.marker([39.73, -104.8]).bindPopup('This is Aurora, CO.').addTo(kavling),
-L.marker([39.77, -105.23]).bindPopup('This is Golden, CO.').addTo(kavling);";
 require('conn/koneksi.php');
 
 $jenis = "P002";
@@ -13,7 +8,6 @@ $db->execute();
 
 $cos = $db->get_result();
 $res = $cos->fetch_all(MYSQLI_ASSOC);
-var_dump($res);
 
 sizeof($res);
 foreach ($res as $key => $sult) {
