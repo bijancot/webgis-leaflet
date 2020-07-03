0049b50733ed85dfc896289cedb54159c548398f
budosen <panjidia995@gmail.com>
2020-07-03 10:04:58 +0700

    asd

--- layer/rmhBersubsidi.php
+++ layer/rmhBersubsidi.php
@@ -1,5 +1,5 @@
 <?php
-require '../conn/koneksi.php';
+
 echo "var greenIcon = new L.Icon({
 	iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
 	shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
