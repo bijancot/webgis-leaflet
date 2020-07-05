9fe9954894e05bf4f3c705b3d2817a5148cec508
budosen <panjidia995@gmail.com>
2020-07-06 00:16:54 +0700

    add new var

--- layer/showAll.php
+++ layer/showAll.php
@@ -21,7 +21,7 @@ $res = $cos->fetch_all(MYSQLI_ASSOC);
 
 $count = 0;
 foreach ($res as $key => $sult) {
-    echo "var part_kav_".$count." = '<h2>Apartement".$sult['NamaProperti']."</h2><div class=\"columns\">'+
+    echo "var part_all_".$count." = '<h2>Apartement".$sult['NamaProperti']."</h2><div class=\"columns\">'+
 		'<div class=\"column\">' +
 		'<img src = \"https://scontent-sin6-2.xx.fbcdn.net/v/t1.0-9/91400513_1553639021452975_5524976870795247616_n.png?_nc_cat=110&_nc_sid=730e14&_nc_eui2=AeF3Z5XcreiARhaAIR3lFBk8i4S9cKEyQ4KLhL1woTJDgjW1y9_p-osrlqnLwrxBLxlX95MDOg9MvMSQ2wqcufoN&_nc_ohc=w6KoWgus7VAAX-dK1mL&_nc_ht=scontent-sin6-2.xx&oh=1b5cea5b065fe627c23d040f57ab790a&oe=5F166FF5\"/>' +
 		'</div>'+
