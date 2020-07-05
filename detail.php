88bd72343cf35a516473150d0860f727c83e688e
budosen <panjidia995@gmail.com>
2020-07-06 02:20:55 +0700

    add new var

--- detail.php
+++ detail.php
@@ -1,6 +1,20 @@
 <?php
 
+require 'conn/koneksi.php';
 $jenisProperti = $_GET['jenis'];
 $idProperti = $_GET['properti'];
 
+
+
+$jenis = "P006";
+$db = $mysqli->prepare("SELECT * FROM properti a join foto_tabel b on a.PropertiID=b.PropertiID where a.PropertiID= ?");
+$db->bind_param("s",$idProperti);
+$db->execute();
+
+$cos = $db->get_result();
+$res = $cos->fetch_all(MYSQLI_ASSOC);
+
+foreach ($res as $key => $value) {
+    echo $value['Foto'];
+}
 ?>
