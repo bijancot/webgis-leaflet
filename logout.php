ed81e25427ed1013948c0e1ed2c307e79096db10
budosen <panjidia995@gmail.com>
2020-07-06 01:48:15 +0700

    add new var

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 session_destroy();
 
-$base_url = "https://kristomoyo.com/leaflet";
+$base_url = "http://kristomoyo.com/leaflet";
 echo ("<script LANGUAGE='JavaScript'>
 window.alert('Terimakasih! :)');
 window.location.href='".$base_url."';
