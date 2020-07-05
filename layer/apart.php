432de903db2f54bd644bfa135d847437ffb7d9b5
budosen <panjidia995@gmail.com>
2020-07-06 01:19:18 +0700

    add new var

--- layer/apart.php
+++ layer/apart.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "var blackIcon = new L.Icon({
-	iconUrl: '../icon/apartment.png',
+	iconUrl: 'icon/apartment.png',
 	shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
 	iconSize: [25, 41],
 	iconAnchor: [12, 41],
