bcf5108c2042763c626f00f62bef824c56204e58
budosen <panjidia995@gmail.com>
2020-07-06 00:32:52 +0700

    add new var

--- layer-contoh.php
+++ layer-contoh.php
@@ -36,10 +36,12 @@
 	
 </head>
 <body>
-<nav class="navbar" role="navigation" aria-label="main navigation">
+<nav class="navbar is-primary" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="https://bulma.io">
-      <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+    <a class="navbar-item" href="#">
+	<h1 class="title">
+        Webgis-Leaflet
+    </h1>
     </a>
 
     <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
@@ -51,38 +53,24 @@
 
   <div id="navbarBasicExample" class="navbar-menu">
     <div class="navbar-start">
-      <a class="navbar-item">
+      <a class="navbar-item" gref="#">
         Home
       </a>
-
       <a class="navbar-item">
-        Documentation
+        Dokumentasi
       </a>
-
-      <div class="navbar-item has-dropdown is-hoverable">
-        <a class="navbar-link">
-          More
-        </a>
-
-        <div class="navbar-dropdown">
-          <a class="navbar-item">
-            About
-          </a>
-          <a class="navbar-item">
-            Jobs
-          </a>
-          <a class="navbar-item">
-            Contact
-          </a>
-          <hr class="navbar-divider">
-          <a class="navbar-item">
-            Report an issue
-          </a>
-        </div>
-      </div>
+	  </div>
+	  <span class="navbar-item">
+                    <a class="button is-primary is-inverted" href="https://github.com/bijancot/webgis-leaflet">
+                      <span class="icon">
+                        <i class="fab fa-github"></i>
+                      </span>
+                      <span>Github</span>
+                    </a>
+                  </span>
     </div>
 
-    <div class="navbar-end">
+    <!-- <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons">
           <a class="button is-primary">
@@ -93,7 +81,7 @@
           </a>
         </div>
       </div>
-    </div>
+    </div> -->
   </div>
 </nav>
 <div id='map'></div>
