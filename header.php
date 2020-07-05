698ed8fa1681607a9b98450f83a76040c12f68e5
budosen <panjidia995@gmail.com>
2020-07-06 01:27:47 +0700

    add new var

---
+++ header.php
@@ -0,0 +1,48 @@
+<nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="#">
+	<h1 class="title is-right">
+        Webgis-Leaflet
+    </h1>
+    </a>
+
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" gref="#">
+        Home
+      </a>
+      <a class="navbar-item">
+        Dokumentasi
+      </a>
+	  </div>
+	  <span class="navbar-item">
+            <a class="button is-primary is-inverted" href="https://github.com/bijancot/webgis-leaflet">
+                <span class="icon">
+                    <i class="fab fa-github"></i>
+                </span>
+                <span>Github</span>
+            </a>
+       </span>
+    </div>
+
+    <!-- <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div> -->
+  </div>
+</nav>
