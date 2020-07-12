fad808d07287c29b8079ae1cfc7bfb198cd6ab12
budosen <panjidia995@gmail.com>
2020-07-13 01:05:38 +0700

    add rupiah function

--- layer/rusun.php
+++ layer/rusun.php
@@ -21,18 +21,18 @@ $res = $cos->fetch_all(MYSQLI_ASSOC);
 
 $count = 0;
 foreach ($res as $key => $sult) {
-    echo "var part_kav_".$count." = '<h2>Rumah Susun".$sult['NamaProperti']."</h2><div class=\"columns\">'+
-		'<div class=\"column\">' +
-		'<img src = \"https://scontent-sin6-2.xx.fbcdn.net/v/t1.0-9/91400513_1553639021452975_5524976870795247616_n.png?_nc_cat=110&_nc_sid=730e14&_nc_eui2=AeF3Z5XcreiARhaAIR3lFBk8i4S9cKEyQ4KLhL1woTJDgjW1y9_p-osrlqnLwrxBLxlX95MDOg9MvMSQ2wqcufoN&_nc_ohc=w6KoWgus7VAAX-dK1mL&_nc_ht=scontent-sin6-2.xx&oh=1b5cea5b065fe627c23d040f57ab790a&oe=5F166FF5\"/>' +
-		'</div>'+
-		'<div class=\"column\">".$sult['NamaProperti']."</div>'+
-	'</div>';\n";
+	echo "var part_kav_".$count." = '<h6 class=\"".$sult['NamaJenis']."\">Jenis Properti : ".$sult['NamaJenis']."</h6>'+
+    '<h3 class=\"title\">".$sult['NamaProperti']."</h3>'+
+    '<p>Alamat Properti : ".$sult['Alamat_properti']."</p>' +
+    '<p>Harga : ".rupiah($sult['Harga'])."</p>' +
+    'Info Lebih lanjut :'+
+    '<a href=\"detail.php?jenis=".$sult['JenisID']."&properti=".$sult['PropertiID']."\">Detail properti</a>'\n";
 
 	
 	echo "var customOptions_kav_".$count." =
         {
-        'maxWidth': '800',
-        'maxheight': '800',
+        'maxWidth': '300',
+        'maxheight': '300',
         'className' : 'custom_kav_".$count,"'
         }\n";
     echo "L.marker([".$sult['Latitude'].",".$sult['Longitude']."], {icon: orangeIcon}).bindPopup(part_kav_".$count,", customOptions_kav_".$count.").addTo(rusun);";
