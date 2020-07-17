d5a5fe90a81624611d9dfcf3c315d00083a44ce5
budosen <panjidia995@gmail.com>
2020-07-17 08:00:46 +0700

    last

--- filter2.php
+++ filter2.php
@@ -56,19 +56,19 @@ session_start();
         <h4>Filter search</h4></br>
         <input type="hidden" name="cari" value="true">
         <div class="columns">
-            <div class="column"><input type="text" name="Alamat_properti" placeholder="alamat"></div>
+            <div class="column is-12"><input type="text" name="Alamat_properti" placeholder="alamat"></div>
         </div>
         <div class="columns">
-            <div class="column"><input type="text" name="Harga" placeholder="Harga"></div>
-            <div class="column"><input type="text" name="Luas" placeholder="Luas"></div>
+            <div class="column is-6"><input type="text" name="Harga" placeholder="Harga"></div>
+            <div class="column is-6"><input type="text" name="Luas" placeholder="Luas"></div>
         </div>
         <div class="columns">
-            <div class="column"><input type="text" name="Jarak" placeholder="Jarak Ke Pusat Kota"></div>
-            <div class="column"><input type="text" name="Jumlah_cicil" placeholder="Jumlah Cicil"></div>
+            <div class="column is-6"><input type="text" name="Jarak" placeholder="Jarak Ke Pusat Kota"></div>
+            <div class="column is-6"><input type="text" name="Jumlah_cicil" placeholder="Jumlah Cicil"></div>
         </div>
         <div class="columns">
-            <div class="column"><input type="text" name="Tahun_bangun" placeholder="Tahun bangun"></div>
-            <div class="column"><input class="button is-primary" type="submit" value="Cari"></div>
+            <div class="column is-6"><input type="text" name="Tahun_bangun" placeholder="Tahun bangun"></div>
+            <div class="column is-6"><input class="button is-primary" type="submit" value="Cari"></div>
         </div>
         
     </form>
